fe3fe6ee2ec205c0bc12df508ee7c65aab6cd284
Piyush <piyush.k.chaudhari@gmail.com>
2016-06-02 19:04:04 +0530

    The code has been cleaned and improved

--- src/marsrover/Plateau.java
+++ src/marsrover/Plateau.java
@@ -2,14 +2,13 @@ package marsrover;
 
 public class Plateau {
 
-
     private Coordinates higherLimit;
 
     public Plateau(Coordinates higherLimit) {
         this.higherLimit = higherLimit;
     }
 
-    public Coordinates getHigherLimit() {
+    Coordinates getHigherLimit() {
         return higherLimit;
     }
 }
